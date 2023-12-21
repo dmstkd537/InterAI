3d8403a2d169a8e151b51fbf4b52eeb9b58954cc
dmstkd537 <153149821+dmstkd537@users.noreply.github.com>
2023-12-21 20:22:52 +0900

    Add files via upload

---
+++ 1221/pratice 4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	for (int i = 0; i <= 9; i++) {
+		for (int j = 1; j <= 9; j++)
+		{
+			cout << j << "X" << i << "=" << i * j << "\t";
+		}
+		cout << endl;
+		{
+
+		}
+	}
+	{
+
+	}
+}
