78797a5fe34a65f32433a50f615b8948a25bce73
dmstkd537 <153149821+dmstkd537@users.noreply.github.com>
2024-01-09 17:36:46 +0900

    Add files via upload

---
+++ 20240109/linux c/catkin.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* 파일 복사 프로그램 */
+int main(int argc, char *argv[])
+{
+char c;
+FILE *fp1, *fp2;
+if (argc !=3) {
+fprintf(stderr, "사용법: %s 파일1 파일2\n", argv[0]);
+return 1;
+	}
+}
