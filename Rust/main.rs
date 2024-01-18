9843e2f250d2325a42031f6c32c007a99ffd525e
dmstkd537 <153149821+dmstkd537@users.noreply.github.com>
2024-01-18 14:23:38 +0900

    Add files via upload

---
+++ Rust/main.rs
@@ -0,0 +1,9 @@
+fn main(){
+    let condition = true;
+    let number = if condition{
+        5
+    } else{
+        6
+    };
+    println!("the value of number is: {}", number);
+} // end of main
