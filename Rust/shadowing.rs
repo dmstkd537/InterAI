7e91e7c32f555674a5aa411c4950e0dbe7a9c7ec
dmstkd537 <153149821+dmstkd537@users.noreply.github.com>
2024-02-28 16:28:45 +0900

    Add files via upload

---
+++ Rust/shadowing.rs
@@ -0,0 +1,15 @@
+fn main(){
+    let x = 5;
+
+    let x = x + 1;
+
+    let x = x * 2;
+
+    let y = 6;
+
+    let z = x * y;
+
+    println!("the value of x is: {}", x );
+    println!("the value of y is: {}", y);
+    println!("the value of z is: {}", z); 
+}
